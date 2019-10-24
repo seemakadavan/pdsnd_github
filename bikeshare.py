dfcbb8f4df1e786782176dbdc296c6f20d46a2e5
Seema Kadavan <skadavan@skadavan-a02.vmware.com>
2019-10-24 14:26:45 +0530

    Added method to calculate mean

--- bikeshare.py
+++ bikeshare.py
@@ -7,4 +7,8 @@ sumOfTravelTime <- function(dataset) {
    sum(dataset$Trip.Duration / 60, na.rm = TRUE) 
 }
 
+# Returns mean of travel time for the dataset
+meanOfTravelTime <- function(dataset) {
+   mean(dataset$Trip.Duration, na.rm = TRUE)
+}
 
